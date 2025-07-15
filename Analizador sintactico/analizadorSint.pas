1e365350ef9fc9c9c7e8936a06131841a37d2830
Mario Caballero <mario.caballero.948@gmail.com>
2025-07-15 12:29:20 -0300

    iniciar Tas mas pruebas

--- Analizador sintactico/analizadorSint.pas
+++ Analizador sintactico/analizadorSint.pas
@@ -1,47 +1,17 @@
 
 Program analizadorSint;
 
+Uses unitInitTAS;
+
 Const 
   var_cant = 28;
   var_simb = 32;
-  maxProduccion = 8;
 
-Type 
-  // t_elem_Pila = Record
-  //   simb: t_simbolo_gramatical;
-  //   nodo: t_arbol_derivacion;
-  // End;
-
-  TipoSimbGramCom = (Tid,Tcreal,Tcentera,Tccadena,Tparentesisabre,
-                     Tparentisiscierra
-                     ,Tmas,Tmenos,TProducto,T_division,Tptoycoma,
-                     Tcoma,
-                     Tpunto,Toperadorrealacional,
-                     Toperadorasignacion,
-                     ErrorLex, Pesos, TparReservada, T_llaveabre,
-                     T_llavecierra, T_oplog, T_corchabre,
-                     T_corchcierra,
-                     Tpot);
-
-  TipoVarPrograma = (Programa,Asignacion,BloqueVar,Exp3,Cond,Exp,Cuerpo,Ciclica,
-                     Exp5,Condicional,Exp6,ListaExp,ListaDefVar,Exp7,Exp4,
-                     ListaDefVar2,Cond2,Condicional2,Cond5,Lectura,Sentencia,
-                     Exp2,Cond3,DefVar,Escritura,ListaExp2,Typo,Cond4);
-
+  // Type 
   // t_pila = Record
   //   elem: array [1..maxPila] Of t_elem_Pila;
   //   tope: word;
   // End;
-
-  t_produccion = Record
-    elem: array [1..maxProduccion] Of TipoSimbGramCom;
-    cant: byte;
-  End;
-
-
-  t_puntero_produccion =  ^t_produccion;
-
-  tipo_TAS = array [TipoVarPrograma,TipoSimboloGramatical] Of ^t_produccion;
 
   // t_arbol_derivacion = ^t_nodo_arbol;
   // t_nodo_arbol = Record
@@ -53,5 +23,13 @@ Type
 
   // End;
 
+Var 
+  TAS: tipo_TAS;
+  i: byte;
 Begin
+  initTAS(TAS);
+
+  For i:= 1 To TAS[Exp3,Tid]^.cant Do
+    writeln(TAS[Exp3,Tid]^.elem[i]);
+
 End.

d928fdf93b3b471bec2d4ee7d8e873a85c8ac9fb
Mario Caballero <mario.caballero.948@gmail.com>
2025-07-17 17:06:51 -0300

    agregado de muestra errores

--- AnalizadorSintactico/analizadorSint.pas
+++ AnalizadorSintactico/analizadorSint.pas
@@ -1,7 +1,6 @@
 
 Program analizadorSint;
 
-
 {$unitPath ./helpers/}
 {$unitPath ../AnalizadorLexico/}
 {$unitPath ../AnalizadorLexico/helpers/}
@@ -22,20 +21,13 @@ Begin
   initTAS(TAS);
   iniciarpila(pila);
   control := 0;
-  complex := eps;
   lexema := '';
   anLex(control, complex, lexema, TS);
-  WriteLn;
-  For i:= 1 To pila.tope Do
-    WriteLn(pila.elem[i].simb);
-  WriteLn;
-  WriteLn('complex: ', complex);
 
   While (complex <> ErrorLex) And (complex <> ErrorSint) And (complex <> Pesos) 
     Do
     Begin
       desapilar(pila, elem);
-      WriteLn('complex: ', complex);
 
       If elem In [T_llaveabre..Pesos] Then // Para cuando es terminal
         Begin
@@ -43,16 +35,21 @@ Begin
             // antes debo guardar el lexema
             anLex(control, complex, lexema, TS)
           Else
-            complex := ErrorSint;
-          // se esperaba tal elem y se encontro complex
+            Begin
+              WriteLn('Se esperaba ', elem, ' y se encontro ',complex);
+              complex := ErrorSint;
+            End;
         End;
 
 
       If elem In [Programa..Cond4] Then  // Para cuando es variable
         Begin
           If TAS[elem,complex] = Nil Then
-            complex := ErrorSint
-                       // desde elem no se puede derivar una cadena con complex
+            Begin
+              WriteLn('Desde ', elem,
+                      ' no se puede derivar una cadena con ', complex);
+              complex := ErrorSint;
+            End
           Else
             For i:= TAS[elem,complex]^.cant Downto 1 Do
               apilar(pila, TAS[elem,complex]^.elem[i]);
@@ -62,19 +59,9 @@ Begin
         If complex <> Pesos Then
           complex := ErrorSint;
 
-      WriteLn;
-      For i:= 1 To pila.tope Do
-        WriteLn(pila.elem[i].simb);
-      WriteLn;
     End;
 
   If complex = ErrorLex Then
     WriteLn('Error lexico: ',lexema, ' es invalido');
-
-  WriteLn('complex: ', complex);
-  WriteLn('Lexema: ', Lexema);
-
-  // For i:= 1 To TAS[ListaDefVar2, complex]^.cant Do
-  //   writeln(TAS[Programa, Tprogram]^.elem[i])
 
 End.
